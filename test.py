c3ee922b008cdb08a373aaa949b2da2405812c73
s13508379 <s1350837@live.hkmu.edu.hk>
2025-07-11 10:05:20 +0800

    test

--- test.py
+++
@@ -1,8 +0,0 @@
-from pymarker.core import generate_patt, generate_marker
-
-def main():
-    filename = "tests/input/hiro.jpg"
-    border_size = 50 
-
-    generate_patt(filename)
-    generate_marker(filename,border_size)
